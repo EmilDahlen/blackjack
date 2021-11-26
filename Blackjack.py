c31116f7f24549bf4d6ee77cb03636f0d284f5df
EmilDahlen <emildahlen.se@gmail.com>
2021-11-26 23:48:59 +0100

    Bug fixes and overall cleaning

--- Blackjack.py
+++ Blackjack.py
@@ -18,29 +18,29 @@ def tuple_to_str(tuple):
 def str_to_tuple(string):
     return tuple(map(func, string.split(', ')))
 
-def func(s: str):
+def func(string):
     try:
-        return int(s)
+        return int(string)
     except ValueError:
-        return s.replace("'", "")
+        return string.replace("'", "")
 
 # A class is collection of objects, data and functions. This class is a class that define what a deck of cards is and what functions it can enact
 class deck:
 
-    def create_deck(self):  # This function creates a list of 52 tuples that symbolize playing cards
+    def __init__(self):
+        self.cards = list()
+        self.create_deck()
+
+     # This function creates a list of 52 tuples that symbolize playing cards
+    def create_deck(self):
         deck = list()
-        card_number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]  # From this row and two rows
-        # down, we have three different characteristics that the cards hold
+        card_number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         card_value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
         card_suit = ["♡", "♤", "♢", "♧"]
         for i in card_suit:
-            for o, number in enumerate(card_number):
-                deck.append((number, i, card_value[o]))  # Appends "cards" to the list with three different values
-        return deck  # Because this is a function, we have a return-value, if we didn't it would be called a method
-        # We return the deck so upon calling the function, we retrieve the local variable "deck"
-
-    def __init__(self):
-        self.cards = deck.create_deck(self)
+            for value, number in zip(card_value, card_number):
+                deck.append((number, i, value)) 
+        return deck 
 
     def rotate_deck(self, shift):
         net_shift = abs(shift) % len(self.cards)
@@ -63,14 +63,13 @@ class deck:
             deck.rotate_deck(self, i + 1)
             return active_card[0]
 
-
 class player:
 
     def __init__(self, money, name):
+        self.name = name
         self.cards = []
         self.money = money
         self.is_dealer = False
-        self.name = name
         self.has_betted = False
         self.has_had_round = False
         self.has_got_dealt = False
@@ -94,8 +93,7 @@ class player:
             if amount <= self.money:
                 self.money -= amount
                 return amount
-            else:
-                print("You don't have enough money to bet: " + str(amount))
+            print("You don't have enough money to bet: " + str(amount))
 
     def show_card(self, card_position):
         active_card = self.cards[card_position]
@@ -140,8 +138,8 @@ class player:
     def get_value(self, card):
         return card[2]
 
-    def count_cards(self): # bug: 4 + 7 + 10 + A = 21. When it should = 22
-        card_count = int()
+    def count_cards(self):
+        card_value = int()
         ace_count = int()
         card_order = self.cards
         card_order.sort(key=self.get_value)
@@ -149,13 +147,13 @@ class player:
             if i[0] == "A":
                 ace_count += 1
             else:
-                card_count += i[2]
-        for i in range(ace_count + 1):
-            if card_count+((ace_count-i)*11)+i > 21:
+                card_value += i[2]
+        for i in range(ace_count):
+            if card_value+((ace_count-i)*11)+i > 21:
                 pass
             else:
-                card_count += ((ace_count-i)*11)+i
-        return card_count
+                card_value += ((ace_count-i)*11)+i
+        return card_value
 
 class game:
     fileDir = os.path.dirname(os.path.realpath(__file__))
@@ -164,9 +162,18 @@ class game:
         if name != "":
             self.load(name)
             return
-        self.name = input("Enter game name: ")
+        while True:
+            self.name = input("Enter game name: ")
+            if "." in self.name:
+                print("Enter a name that doesn't contain a \".\"")
+                pass
+            if "/" in self.name:
+                print("Enter a name that doesn't contain a \"/\"")
+                pass
+            else:
+                break
         self.file_name = os.path.join(self.fileDir, "Saves", self.name)
-        self.file = open(self.file_name, "w+", encoding="utf-8")
+        self.file = open(self.file_name, "w+", encoding = "utf-8")
         self.deck = deck()
         self.deck.shuffle_deck()
         self.player_list = []
@@ -217,7 +224,13 @@ class game:
         self.play_game()
 
     def write_player(self, player, i):
-        self.file.write(player.name + ";" + str(player.money) + ";" + str(player.has_betted) +  ";" + str(player.has_had_round) + ";" + str(player.has_got_dealt)+ ";" + str(player.game_done))
+        self.file.write(
+            player.name + ";" + 
+            str(player.money) + ";" + 
+            str(player.has_betted) +  ";" + 
+            str(player.has_had_round) + ";" + 
+            str(player.has_got_dealt)+ ";" + 
+            str(player.game_done))
         for card in player.cards:
             self.file.write(";" + tuple_to_str(card))
         self.file.write(";" + str(self.player_bets[i]))
@@ -235,7 +248,12 @@ class game:
 
 
     def add_player_m(self):
-        player_name = input("Enter player name: ")
+        while True:
+            player_name = input("Enter player name: ")
+            if ";" in player_name:
+                print("Enter a name that doesn't contain a \";\"")
+            else:
+                break
         while True:
                 player_money = input("Enter player balance: ")
                 try:
@@ -343,31 +361,32 @@ class game:
                 else:
                     print(str(player.name) + ": LOSE\nYou lost: " + str(self.player_bets[i]))
             self.write_state()
+        
 
     def play_game(self):
         self.start()
         self.round()
         self.winner()
+        self.dealer.has_betted = False
+        self.dealer.has_had_round = False
+        self.dealer.has_got_dealt = False
+        self.dealer.game_done = False
+        self.file.truncate(0)
+        self.file.seek(0)
         self.deck.shuffle_deck()
         self.dealer.cards = []
         rematch = []
-        for mndex, player in enumerate(self.player_list):
+        for _, player in enumerate(self.player_list):
             player.cards = []
+            player.has_betted = False
+            player.has_had_round = False
+            player.has_got_dealt = False
+            player.game_done = False
             while True:
                 if player.money <= 0:
                     break
                 ask_rematch = input(player.name + " do you want to play again? Yes or No\n").lower()
                 if ask_rematch == "yes":
-                    player.has_betted = False
-                    player.has_had_round = False
-                    player.has_got_dealt = False
-                    player.game_done = False
-                    self.dealer.has_betted = False
-                    self.dealer.has_had_round = False
-                    self.dealer.has_got_dealt = False
-                    self.dealer.game_done = False
-                    self.file.truncate(0)
-                    self.file.seek(0)
                     rematch.append(player)
                     break
                 if ask_rematch == "no":
